81c453261c9bcc425a08d728e3795a24a8e67471
shreyaszadge <zadgeshreyas@gmail.com>
2024-04-22 09:49:25 +0530

    complete the post card of the feed section

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,8 +1,23 @@
 // import React from 'react'
-
-const Posts = () => {
+import {app} from '../firebase'
+import { getFirestore,query,collection,orderBy, getDocs } from 'firebase/firestore';
+import Post from './Post';
+const Posts =async () => {
+  const db=getFirestore(app);
+  const q=query(collection(db,'posts'),orderBy('TimeStamp','desc'));
+  const querySnapshot=await getDocs(q);
+  let data= [];
+  querySnapshot.forEach((doc)=>{
+    data.push({id:doc.id,...doc.data()})
+  })
+// console.log(data)
+  
   return (
-    <div>posts</div>
+    <div>
+      {data.map((post)=>(
+        <Post key={post.id} post={post} ></Post>
+      ))}
+    </div>
   )
 }
 
